f2170cc70509905ae59c9f9ee820e2928f4b5af1
Sean Smith <smith.sean1998@gmail.com>
2021-12-19 17:42:47 +0000

    finished personal page

--- frontend/components/HeaderMain.vue
+++ frontend/components/HeaderMain.vue
@@ -8,7 +8,7 @@
         <ul class="flex text-white">
           <a href="/work"><li :class="activeRoute === '/work' ? 'active__link text-white px-6' : 'text-white px-6'">Work</li></a>
           <a href="/personal"><li :class="activeRoute === '/personal' ? 'active__link text-white px-6' : 'text-white px-6'">Personal</li></a>
-          <a href=""><li :class="activeRoute === '/about' ? 'active__link text-white px-6' : 'text-white px-6'">About</li></a>
+          <a href="/about"><li :class="activeRoute === '/about' ? 'active__link text-white px-6' : 'text-white px-6'">About</li></a>
           <a href=""><li :class="activeRoute === '/Testimonials' ? 'active__link text-white px-6' : 'text-white px-6'">Testimonials</li></a>
           <a href=""><li :class="activeRoute === '/contact' ? 'active__link text-white px-6' : 'text-white px-6'">Contact</li></a>
           <a href=""><li :class="activeRoute === '/prints' ? 'active__link text-white px-6' : 'text-white px-6'">Prints</li></a>
